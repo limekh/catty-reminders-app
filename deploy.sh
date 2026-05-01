0c09661caa0fcbe7996f10ff84753bda72feff76
limekh <sdgright@yandex.ru>
2026-05-01 22:14:08 +0400

    lab3: edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,14 @@ echo "STOP old container"
 docker stop $APP_NAME || true
 docker rm $APP_NAME || true
 
+DEPLOY_REF=$(git rev-parse HEAD)
+echo "DEPLOY_REF=$DEPLOY_REF"
+
 echo "RUN new container"
 docker run -d \
   --name $APP_NAME \
   -p 8181:8181 \
-  -e DEPLOY_REF=(git rev-parse HEAD) \
+  -e DEPLOY_REF=$DEPLOY_REF \
   $IMAGE
 
 echo "Deploy done"
